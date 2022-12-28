9bfb272a2c06a3386dc0e48b35481b3e52e95d08
shishircg <sishirgautam01@gmail.com>
2022-12-28 13:26:19 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let js = "amazing";
+if (js === "amazing") alert("javascript is fun");
+console.log(40 + 20 + 25 - 5);
+console.log("jonas");
+console.log("23");
+
+let firstname = "jonas";
+console.log(firstname);
+
+let myFirstjob = 'Programmer';
+//variable name convection 
